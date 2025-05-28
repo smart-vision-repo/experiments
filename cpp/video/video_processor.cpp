70d60b0ca4e3f58dacb476f1418e5009960f6d86
tju <tony.ju@me.com>
2025-05-29 00:15:57 +0800

    Refactor VideoProcessor to ensure proper clearing of AV packets after decoding. This change enhances the accuracy of frame processing and maintains the integrity of the packet list during GOP handling.

--- cpp/video/video_processor.cpp
+++ cpp/video/video_processor.cpp
@@ -95,20 +95,19 @@ int VideoProcessor::process() {
                     total_hits += hits;
                     pool = frame_idx_in_gop - last_frame_in_gop;
                     std::vector<AVPacket *> decoding_pkts = get_packets_for_decoding(pkts, last_frame_in_gop);
-                    decoder.reset();
                     decoder.decode(decoding_pkts, interval);
                     std::vector<cv::Mat> decoded_frams = decoder.getDecodedFrames();
                     success += decoded_frams.size();
                     std::cout << "decoded: " << decoded_frams.size() << std::endl;
                     total_packages += decoding_pkts.size();
                     clear_av_packets(&decoding_pkts);
-                    clear_av_packets(pkts);
                 } else {
                     pool += frame_idx_in_gop;
                 }
                 frame_idx_in_gop = 0;
                 hits = 0;
                 gop_idx++;
+                clear_av_packets(pkts);
                 add_av_packet_to_list(&pkts, packet);
             } else {
                 if (pkts->size() > 0) {
